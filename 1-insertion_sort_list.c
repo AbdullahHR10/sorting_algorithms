c833ee1842cfa03bd3a41710cbfd2b5b480bc737
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-11 06:47:27 -0700

    Created the file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of ints in ascending order
+ * Using the Insertion sort algorithm
+ * @list: To be sorted
+ *
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
